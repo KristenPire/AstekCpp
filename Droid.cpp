b964acb82a6b268b6180f2bd5cf9af2fc48dcb71
cmyGirard <cmy.girard@gmail.com>
2018-12-04 15:41:29 +0100

    getter setter, construteur et copy constructeur

--- Droid.cpp
+++ Droid.cpp
@@ -1,2 +1,65 @@
 #include "Droid.hh"
 
+// constructeurs et destructeur
+
+
+Droid::Droid(std::string serial=""):energy(50), attack(25), toughness(15)
+{
+	*status ="Standing by";
+	id=serial;
+};
+
+Droid::Droid(const Droid& droid):energy(50), attack(25), toughness(15){
+	*status=*(droid.status);
+	id=droid.id;
+}
+
+
+Droid::~Droid(){
+
+};
+
+
+// getter et setteur
+
+std::string Droid::getId(){
+	return id;
+};
+
+size_t Droid::getEnergy(){
+	return energy;
+};
+
+size_t Droid::getAttack(){
+	return attack;
+};
+
+size_t Droid::getToughness(){
+	return toughness;
+};
+
+std::string* Droid::getStatus(){
+	return status;
+};
+
+void Droid::setId(std::string newId){
+	id=newId;
+};
+
+void Droid::setEnergy(size_t newEnergy){
+	energy=newEnergy;
+};
+
+void Droid::setStatus(std::string* newStatus){
+	status=newStatus;
+};
+
+
+int main(){
+
+Droid d1=Droid();
+Droid d2=Droid("d2");
+
+
+return 0;
+};
--- Droid.hh
+++ Droid.hh
@@ -1,2 +1,29 @@
 #include <iostream>
 #include <string>
+
+class Droid{
+public:
+	Droid(std::string serial);
+	Droid (const Droid& );
+	~Droid();
+
+	std::string getId();
+	size_t getEnergy();
+	size_t getAttack();
+	size_t getToughness();
+	std::string* getStatus();
+
+	void setId(std::string newId);
+	void setEnergy(size_t newEnergy);
+	void setStatus(std::string* newStatus);
+
+
+private:
+	size_t energy;
+	const size_t attack;
+	const size_t toughness;
+	std::string id;
+	std::string* status;
+
+};
+
