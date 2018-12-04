b97f0873da33d02307db0cb6a881a57039154053
Alexis Lagrange <alexis-lagrange@protonmail.com>
2018-12-04 14:36:00 +0100

    wip before ssh

--- Droid.cpp
+++ Droid.cpp
@@ -1,1 +1,30 @@
+#include "Droid.hh"
+
+Droid::Droid(std::string serial=""): Id(serial), Energy(50), Attack(25), Toughness(15), Status("Standing by")
+{
+	std::cout << "Droid \'"  << this->Id << " \' Activated" <<  std::endl;
+}
+
+Droid::~Droid(){
+	std::cout << "Droid \'"  << this->Id << " \' Destroyed" <<  std::endl;
+}
+
+int main(){
+	Droid d;
+	Droid d1("Avenger");
+	size_t Durasel = 200;
+	
+	Std::cout << d << std::endl;
+	Std::cout << d1 << std::endl;
+	d = d1;
+	d.setStatus(new std::string("Kill Kill Kill!"));
+	d << Durasel;
+	std::cout << d << "--" << Durasel << std::endl;
+	Droid d2 = d;
+	d.setId("Rex");
+	std::cout << (d2 != d) << std::endl;
+	
+	return 0;
+}
+
 
--- Droid.hh
+++ Droid.hh
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
 
+class Droid {
+	private:
+		std::string Id;
+		size_t Energy;
+		const size_t Attack;
+		const size_t Toughness;
+		std::string *Status;
+		
+	public:
+		Droid(std::string serial);
+		~Droid();
+		std::string getId();
+		size_t getEnergy();
+		const size_t getAttack();
+		const size_t getToughness();
+		std::string *getStatus();
+		
+		std::string setId();
+		size_t setEnergy();
+		std::string *setStatus();
+}
