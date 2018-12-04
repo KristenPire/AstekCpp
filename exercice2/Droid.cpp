534d7c163b27448811500e52106d54f094e6b0b2
Hippo F <you@example.com>
2018-12-04 14:32:01 +0100

    version1 aprem

---
+++ exercice2/Droid.cpp
@@ -0,0 +1,27 @@
+#include "Droid.hh"
+#include <iostream>
+#include <string>
+
+Droid::Droid(const std::string serial) : _Attack(25), _Toughness(15)
+{
+	_ID = serial;
+	_Energy = 50;
+	_Status = "Standing by"
+}
+
+Droid::Droid(Droid &Source) : _Attack(Source._Attack), _Toughness(Source._Toughness)
+{
+	_ID = Source._ID;
+	_Energy = Source._Energy;
+	_Status = Source._Status;
+}
+
+Droid::~Droid()
+{
+	std::cout<<"Destruction "<<this->_ID<<std::endl;
+}
+
+void Droid::status()
+{
+	std::cout<<"Droid "<<this->_ID<<" "<<this->_Status<<std::endl;
+}
