bb6d77e8980449e3278cbc24b56d69947392adf2
Alexis Lagrange <alexis-lagrange@protonmail.com>
2018-12-04 11:59:36 +0100

    Code should now work

--- Locale/Locale.cpp
+++ Locale/Locale.cpp
@@ -1,1 +1,81 @@
+#include "Locale.hh"
 
+Locate::Locate(int x_, int y_, int serial_): serial(serial_), x(x_), y(y_), linked_locate(NULL)  //Utilisation d' init list pour definir serial malgré le static
+{
+	std::cout << "Locate " << this->serial << " initialised" << std::endl;
+}
+
+Locate::~Locate(){
+	std::cout << "Locate " << this->serial << " shutting down" << std::endl;
+}
+
+void Locate::addCom(int x_, int y_, int serial_){
+	
+	Locate *L = new Locate(x_, y_, serial_);
+	L->linked_locate = this->linked_locate;
+	this->linked_locate = L;
+	
+}
+
+Locate* Locate::getCom(){
+	return(this->linked_locate);
+}
+
+void Locate::removeCom(){
+	Locate *l = this->linked_locate;
+	this->linked_locate = l->getCom();
+	delete l;
+	
+}
+
+const void Locate::ping(){
+	std::cout << "Locate " << this->serial << " currently at " << x << " " << y << std::endl;
+}
+
+const void Locate::locateSquad(){
+	//~ Locate *l = this->linked_locate;
+	//~ std::cout << "Checking " << this->serial << std::endl;
+	//~ if(l){ //Should be false if pointer is null
+		//~ std::cout << "Moving on" << std::endl;
+		//~ l->locateSquad();
+	//~ }
+	//~ std::cout << "Display "<< this->serial << std::endl;
+	//~ this->ping();
+	
+	//~ bool is_origin = true;
+	//~ Locate *l = this->linked_locate;
+	//~ if(l){
+		//~ l->ping();
+	//~ }
+	//~ else{
+		
+	//~ }
+	//~ l-> locateSquad();
+	
+	//~ this->ping();
+	
+	Locate * tmp;
+	tmp = this->linked_locate;
+	while(tmp !=NULL){
+		tmp->ping();
+		tmp = tmp->linked_locate;
+	}
+	this->ping();
+}
+
+
+int main(){
+	Locate k(42, 32, 101010);
+	
+	k.addCom(56, 25, 65);
+	k.addCom(73, 34, 51);
+	std::cout << "--------------Display the links--------------" << std::endl;
+	k.locateSquad();
+	std::cout << "--------------Remove 51--------------" << std::endl;
+	k.removeCom();
+	std::cout << "--------------Should have removed 51, Let's Check--------------" << std::endl;
+	k.locateSquad();
+	k.removeCom();
+
+	return 0;
+}
--- Locale/Locale.hh
+++ Locale/Locale.hh
@@ -6,10 +6,14 @@ class Locate{
 		const int serial;
 		int x;
 		int y;
+		Locate *linked_locate;
 
 	public:
 		Locate(int x_, int y_, int serial_);
+		~Locate();
 		void addCom(int x_, int y_, int serial_);
 		Locate *getCom();
-		void informations();
+		void removeCom();
+		const void ping();
+		const void locateSquad();
 };
