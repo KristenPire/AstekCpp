2351423f2d5eacae8ba714d448c530d6be02f928
Mildred34 <alexis-h-34@hotmail.fr>
2018-12-04 14:40:13 +0100

    Modif cpp

--- Droid.cpp
+++ Droid.cpp
@@ -1,1 +1,80 @@
 #include "Droid.hh"
+
+Droid::Droid(int serial = "",size_t Energy = 50, size_t Attack = 25, size_t Toughness 15, std::string * Status = "Standing by"){
+
+
+
+}
+
+Droid::Droid(const Droid& A){
+
+
+
+
+}
+
+
+Droid::~Droid(){
+
+
+
+}
+
+
+const void Droid::Id(){
+
+
+
+}
+
+const int Droid::getSerial(){
+
+
+}
+
+const size_t Droid::getEnergy(){
+
+
+}
+
+const size_t Droid::getAttack(){
+
+
+}
+
+const size_t Droid::getToughness(){
+
+
+}
+
+const string * Droid::getStatus(){
+
+
+}
+
+std::string * Droid::setStatus(std::string & Status){
+
+
+}
+
+size_t Droid::setEnergy(size_t Energy){
+
+
+
+}
+
+bool Droid::operator==(const Droid& A){
+
+
+}
+
+bool Droid::operator!=(const Droid& A){
+
+	
+}
+
+ostream& Droid::operator<<(ostream& a, Droid& D){
+
+
+
+}
