63e28631a0de3b2f45e1c2084226f4cf9a5945b5
Alexis Lagrange <alexis-lagrange@protonmail.com>
2018-12-05 09:31:14 +0100

    First commit

---
+++ Sorcerer.hpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+class Sorcerer{
+	private:
+		std::string name;
+		std::string title;
+
+	public:
+		Sorcerer(std::string name_, td::string title_);
+		~Sorcerer();
+};
