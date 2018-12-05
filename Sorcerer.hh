f3b57f5168a08ebc77f06ecd0645c5f9e112d9dd
Takeshi-Edo <yanebout@gmail.com>
2018-12-05 10:06:03 +0100

    Sorcerer class is just finished

--- Sorcerer.hh
+++ Sorcerer.hh
@@ -2,5 +2,22 @@
 #define SORCERER_HH
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+class Sorcerer{
+	public:
+		Sorcerer(std::string name = "Ro/b/ert", std::string title = "the Magnificient");
+		~Sorcerer();
+
+		std::string getName() const;
+		std::string getTitle() const;
+
+	protected:
+		std::string _name;
+		std::string _title;
+};
+
+std::ostream& operator<<(std::ostream& os, const Sorcerer& sorcerer);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Sorcerer.hh"
 
 int main(){
-	
+	Sorcerer s;
+	std::cout << s << std::endl;
 	return 0;
 }
