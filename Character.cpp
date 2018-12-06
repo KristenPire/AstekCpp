f54b8678f028db30e9b33286939ef4aebe6318ac
cmyGirard <cmy.girard@gmail.com>
2018-12-06 11:58:46 +0100

    creation de la class character, son constructeurs et premiers attributs

--- Character.cpp
+++ Character.cpp
@@ -1,1 +1,5 @@
 #include "Character.hpp"
+
+Character::Character(std::string name, int level):_name(name),_level(level){
+
+};
--- Character.hpp
+++ Character.hpp
@@ -4,5 +4,14 @@
 #include <iostream>
 #include <string>
 
+class Character{
+private:
+	Character(std::string name, int level);
+	virtual ~Character()=default;
+public:
+	std::string _name;
+	int _level;
+};
+
 
 #endif
