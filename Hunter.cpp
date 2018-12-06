1be677c5007c875c8f3539b9f9040554a68a5b61
cmyGirard <cmy.girard@gmail.com>
2018-12-06 17:44:11 +0100

    creations des fonctions internes de warrior

--- Hunter.cpp
+++ Hunter.cpp
@@ -1,1 +1,27 @@
 #include "Hunter.hpp"
+
+Hunter::Hunter(std::string name, int level):Character(name,level),Warrior(name, level, "sword"){
+	std::cout<< _name <<"is born from a tree"<<std::endl;
+	_stamina=9;
+	_spirit=6;
+	_agility=25;
+	_strength=9;
+	_intelligence=5;
+	_race="Elf";
+};
+
+void Hunter::RestorePower(){
+	_energy=100;
+	std::cout<<_name<< "meditates"<<std::endl;
+};
+
+int Hunter::RangeAttack(){
+	if (_energy>=25){
+		_energy-=25;
+		std::cout<< _name<<" uses his bow "<<std::endl;
+		return 20+_agility;
+	}else {
+		std::cout<<" attack failed, not enough energy"<<std::endl;
+		return 0;
+	}
+};
--- Hunter.hpp
+++ Hunter.hpp
@@ -1,8 +1,18 @@
 #ifndef HUNTER_HPP
 #define HUNTER_HPP
 
-include <iostream>
+#include <iostream>
 #include <string>
+#include "Warrior.hpp"
+
+class Hunter:public Warrior{
+public:
+	Hunter(std::string name, int level);
+	virtual~Hunter()=default;
+	virtual void RestorePower();
+	virtual int RangeAttack();
+
+};
 
 
 
