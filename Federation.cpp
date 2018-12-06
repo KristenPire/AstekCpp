ca6c2615b1209308ea7244c2cce174d2ae629ed4
cmyGirard <cmy.girard@gmail.com>
2018-12-06 11:13:26 +0100

    creation du ship exterieur, correction des fonctions d'affichage , creation de ship.checkCore et test du projet

--- Federation.cpp
+++ Federation.cpp
@@ -28,11 +28,50 @@ short Federation::Starfleet::Ship::getMaxWrap()const{
 
 void Federation::Starfleet::Ship::setupCore (WarpSystem::Core * core){
 	_core=core;
+	std::cout<<"USS "<< _name <<" :  The core is set ."<<std::endl;
+};
+
+void Federation::Starfleet::Ship::checkCore()
+{
 	std::cout<<"USS "<< _name <<" :  The core is ";
-	if (core->checkReactor()->isStable()){
+	if (_core->checkReactor()->isStable()){
 		std::cout<< "stable at the time ."<<std::endl;
 	}else{
 		std::cout<< "unstable at the time ."<<std::endl;
 	}
+}
+
+// creation d'un bateau en dehors de starfleet
+
+Federation::Ship::Ship(int length, int width,std::string name):_length(length),_width(width),_name(name),_maxWrap(1){
+	_core=NULL;
+	std::cout<< " The independant ship " << _name << " just finished its construction . It is "<< _length << "m in length and " << _width << "m in width ."<<std::endl;
 };
 
+int Federation::Ship::getLength()const{
+	return _length;
+};
+
+int Federation::Ship::getWidth()const{
+	return _width;
+};
+
+const std::string & Federation::Ship::getName()const{
+	return _name;
+};
+
+
+void Federation::Ship::setupCore (WarpSystem::Core * core){
+	_core=core;
+	std::cout<< _name <<" :  The core is set ."<<std::endl;
+};
+
+void Federation::Ship::checkCore()
+{
+	std::cout<< _name <<" :  The core is ";
+	if (_core->checkReactor()->isStable()){
+		std::cout<< "stable at the time ."<<std::endl;
+	}else{
+		std::cout<< "unstable at the time ."<<std::endl;
+	}
+}
--- Federation.hpp
+++ Federation.hpp
@@ -19,6 +19,7 @@ namespace Federation{
 			const std::string & getName() const;
 			short getMaxWrap()const;
 			void setupCore (WarpSystem::Core * core);
+			void checkCore ();
 
 		private:
 			const int _length;
@@ -27,6 +28,24 @@ namespace Federation{
 			const short _maxWrap;
 			WarpSystem::Core *_core;
 		};
+	};
+
+	class Ship{
+		public:
+			Ship(int length, int width,std::string name);
+			virtual ~Ship()=default;
+			int getLength() const;
+			int getWidth() const;
+			const std::string & getName() const;
+			void setupCore (WarpSystem::Core * core);
+			void checkCore ();
+
+		private:
+			const int _length;
+			const int _width;
+			const std::string _name;
+			const short _maxWrap;
+			WarpSystem::Core *_core;
 	};
 };
 
