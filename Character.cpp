3d48b54fc975e997fa7991746734dc45a2671b29
Mildred34 <alexis-h-34@hotmail.fr>
2018-12-06 14:26:05 +0100

    Exo 0 terminé

--- Character.cpp
+++ Character.cpp
@@ -2,10 +2,13 @@
 
 
 
-Character::Character(const std::string &name, int level):_name(name),_level(level){
+Character::Character(const std::string &name,const std::string& race, int level):_name(name),_race(race),_level(level),_PV(MAX),_power(MAX),_range(CLOSE){
 
-	_PV = MAX;
-	_power = MAX;
+	_carac[Strength] = 5;
+	_carac[Stamina] = 5;
+	_carac[Intelligence] = 5;
+	_carac[Spirit] = 5;
+	_carac[Agility] = 5;
 
 }
 
@@ -18,13 +21,13 @@ const std::string&  Character::getName() const{
 }
 
 
-const int Character::getLvl() const{
+int Character::getLvl() const{
 
 	return _level;
 }
 
 
-const int Character::getPV() const{
+int Character::getPV() const{
 
 	return _PV;
 
@@ -32,10 +35,66 @@ const int Character::getPV() const{
 
 
 
-cont int Character::getPower() const{
+int Character::getPower() const{
 
 
 	return _power;
 }
 
 
+int Character::CloseAttack(){
+
+	if(_power < 10)
+	{
+
+		std::cout << _name << " is too tired to attack " << std::endl;
+		return 0;
+	}
+		std::cout << _name << " strikes with a wooden stick" << std::endl;
+	return 10+_carac[Strength];
+
+}
+
+int Character::RangeAttack(){
+
+	if(_power < 10)
+	{
+
+		std::cout << _name << " is too tired to attack " << std::endl;
+		return 0;
+	}
+		std::cout << _name << " tosses a stone" << std::endl;
+	
+	return 5+_carac[Strength];
+
+}
+
+void Character::Heal(){
+
+	std::cout << _name << " takes a potion" << std::endl;
+	
+	_PV = std::min(MAX, _PV+50);
+
+}
+
+void Character::RestorePower(){
+
+
+	std::cout << _name << " eats some fried chicken" << std::endl;
+	
+	_power = std::min(MAX,100+_power);
+
+}
+
+
+void Character::TakeDamage(int damage){
+
+	_PV = std::max(0, _PV - damage) ;
+	std::cout << _name << " takes " << damage << " damage" << std::endl;
+	
+	if(_PV == 0){
+
+		std::cout << _name << " out of combat" << std::endl;
+
+	}
+}
--- Character.hpp
+++ Character.hpp
@@ -3,27 +3,38 @@
 
 #include <iostream>
 #include <string>
+#include <array>
 
 
 #define MAX 100
 
+
 class Character
 {
-
-
-private:
-	const std::string _name;
-	int _level;
+protected:
+	const std::string _race;
 	int _PV;
 	int _power;
+	std::array<int, 5> _carac;
+	
 
 public:
-	Character(const std::string &name, int level);
+	const std::string _name;
+	int _level;
+	enum AttackRange {CLOSE,RANGE};
+	enum carac {Strength,Stamina,Intelligence,Spirit,Agility};
+	AttackRange _range;
+	Character(const std::string &name,const std::string& race, int level);
 	virtual ~Character()=default;
 	const std::string& getName() const;
 	int getLvl() const;
 	int getPV() const;
 	int getPower() const;
+	int CloseAttack();
+	int RangeAttack();
+	void Heal();
+	void RestorePower();
+	void TakeDamage(int damage);
 	
 };
 
