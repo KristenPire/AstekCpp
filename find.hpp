8862a8517da5551bf9088675e45b77f5cd74e9bb
cmyGirard <cmy.girard@gmail.com>
2018-12-07 11:21:45 +0100

    fin de l'exercce 0

--- find.hpp
+++ find.hpp
@@ -1,8 +1,14 @@
 #ifndef FIND_HPP
 #define FIND_HPP
 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 
-
+template<typename Type>
+typename Type::iterator const do_find(Type & refToTemplate, int integer){
+	return  std::find(std::begin(refToTemplate),std::end(refToTemplate),integer);
+};
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 
 #include <iostream>
+#include "find.hpp"
+#include <vector>
 
 
 int main(){
+
+	
+	std::vector<int> v;
+	v={1,2,3,4,5};
+
+	std::vector<int>::iterator it2;
+
+
+	it2=do_find(v,3);
+	
+
+	std::cout<< *it2<<std::endl;
+	std::cout<< &it2<<std::endl;
+	std::cout<<std::distance(v.begin(),do_find(v,3))<<std::endl;
+
+
+	it2=do_find(v,10);
+	
+
+	std::cout<< *it2<<std::endl;
+	std::cout<< &it2<<std::endl;
+	std::cout<<std::distance(v.begin(),do_find(v,10))<<std::endl;
+
+
+
 
 	return 0;
 }
