14c7a02e39b4f8c24c85bc5007f4455fc8b2a8c7
Alexis Lagrange <alexis-lagrange@protonmail.com>
2018-12-07 11:03:31 +0100

    Now works up to step 04

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ static bool isGreaterThan(int a, int b)
 
 static void showMe(int i)
 {
-		std::cout << std::setw(4) << i << ``, ``;
+		//~ std::cout << std::setw(4) << i << ``, ``;
+		std::cout << std::setw(4) << i << ", ";
 }
 
 static void mult2(int& i)
@@ -47,67 +48,67 @@ int main()
 		vAssign(v1.begin() + 3, v1.begin() + 5, -421);
 		vPrint(v1, &showMe);
 
-		std::cout << "============ Step 05 ==========" << std::endl;
-		vPrint(v1, &showMe);
-		vApply(v1.begin() + 1, v1.begin() + 10, &mult2);
-		vPrint(v1, &showMe);
+		//~ std::cout << "============ Step 05 ==========" << std::endl;
+		//~ vPrint(v1, &showMe);
+		//~ vApply(v1.begin() + 1, v1.begin() + 10, &mult2);
+		//~ vPrint(v1, &showMe);
 
-		std::cout << "============ Step 06 ==========" << std::endl;
-		vPrint(v1, &showMe);
-		it = vGiveMeTheFirst(v1, 84);
-		if (it != v1.end())
-				v1.erase(it, it + 3);
-		vPrint(v1, &showMe);
+		//~ std::cout << "============ Step 06 ==========" << std::endl;
+		//~ vPrint(v1, &showMe);
+		//~ it = vGiveMeTheFirst(v1, 84);
+		//~ if (it != v1.end())
+				//~ v1.erase(it, it + 3);
+		//~ vPrint(v1, &showMe);
 
-		std::cout << "============ Step 07 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		vFlip(v2);
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 07 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ vFlip(v2);
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 08 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		std::vector<int>::iterator newEnd = vRemoveDuplicate(v2);
-		v2.resize(newEnd - v2.begin());
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 08 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ std::vector<int>::iterator newEnd = vRemoveDuplicate(v2);
+		//~ v2.resize(newEnd - v2.begin());
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 09 ==========" << std::endl;
-		vPrint(v1, &showMe);
-		vToAscOrder(v1);
-		vPrint(v1, &showMe);
+		//~ std::cout << "============ Step 09 ==========" << std::endl;
+		//~ vPrint(v1, &showMe);
+		//~ vToAscOrder(v1);
+		//~ vPrint(v1, &showMe);
 
-		std::cout << "============ Step 10 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		vToSpecificOrder(v2, &isGreaterThan);
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 10 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ vToSpecificOrder(v2, &isGreaterThan);
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 11 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		vShift(v2, 3);
-		vPrint(v2, &showMe);
-		vShift(v2, 7);
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 11 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ vShift(v2, 3);
+		//~ vPrint(v2, &showMe);
+		//~ vShift(v2, 7);
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 12 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		vFindAndModify(v2, 21, 777);
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 12 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ vFindAndModify(v2, 21, 777);
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 13 ==========" << std::endl;
-		vPrint(v2, &showMe);
-		newEnd = vFindAndKill(v2, 777);
-		v2.resize(newEnd - v2.begin());
-		vPrint(v2, &showMe);
+		//~ std::cout << "============ Step 13 ==========" << std::endl;
+		//~ vPrint(v2, &showMe);
+		//~ newEnd = vFindAndKill(v2, 777);
+		//~ v2.resize(newEnd - v2.begin());
+		//~ vPrint(v2, &showMe);
 
-		std::cout << "============ Step 14 ==========" << std::endl;
-		std::vector<int> v3(v1.size() + v2.size());
+		//~ std::cout << "============ Step 14 ==========" << std::endl;
+		//~ std::vector<int> v3(v1.size() + v2.size());
 
-		vToAscOrder(v1);
-		vToAscOrder(v2);
-		vPrint(v1, &showMe);
-		vPrint(v2, &showMe);
-		vPrint(v3, &showMe);
-		vFusionOrderedLists(v1, v2, v3);
-		vPrint(v3, &showMe);
+		//~ vToAscOrder(v1);
+		//~ vToAscOrder(v2);
+		//~ vPrint(v1, &showMe);
+		//~ vPrint(v2, &showMe);
+		//~ vPrint(v3, &showMe);
+		//~ vFusionOrderedLists(v1, v2, v3);
+		//~ vPrint(v3, &showMe);
 
 		return 0;
 }
